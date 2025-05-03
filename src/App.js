f4faf8ea341578577ac94c3c0787e457517197ca
Snehajoyce <snehajoyce22@gmail.com>
2025-05-03 12:45:13 -0400

    Refactor(ui): remove Upload and Generate features from UI

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Home from './pages/Home';
-import Upload from './pages/Upload';
 import Search from './pages/Search';
 import Compare from './pages/Compare';
-import Generate from './pages/Generate';
 import SBOMDetails from './pages/SBOMDetails';
 import Statistics from './pages/Statistics';
 
@@ -25,8 +23,6 @@ function App() {
             <h2 className="text-warning m-0">SBOM Finder</h2>
             <nav>
               <Link className="btn btn-outline-light mx-1" to="/">Home</Link>
-              <Link className="btn btn-outline-light mx-1" to="/generate">Generate</Link>
-              <Link className="btn btn-outline-light mx-1" to="/upload">Upload</Link>
               <Link className="btn btn-outline-light mx-1" to="/search">Search</Link>
               <Link className="btn btn-outline-light mx-1" to="/compare">Compare</Link>
               <Link className="btn btn-outline-light mx-1" to="/statistics">Statistics</Link>
@@ -37,8 +33,6 @@ function App() {
         <main className="container mb-5">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/generate" element={<Generate onGenerationSuccess={handleGenerationSuccess} />} />
-            <Route path="/upload" element={<Upload />} />
             <Route path="/search" element={<Search />} />
             <Route path="/compare" element={<Compare />} />
             <Route path="/sbom-details/:filename" element={<SBOMDetails />} />
